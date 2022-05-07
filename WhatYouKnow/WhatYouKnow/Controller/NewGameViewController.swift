08d8b2f28bc3330fef18ce550cd8fb4b3cfae5d2
Borislav <borislav@Borislavs-Mac.local>
2022-05-07 16:29:36 +0300

    Add new functionality for helping from public

--- WhatYouKnow/WhatYouKnow/Controller/NewGameViewController.swift
+++ WhatYouKnow/WhatYouKnow/Controller/NewGameViewController.swift
@@ -19,24 +19,27 @@ class NewGameViewController: UIViewController {
     @IBOutlet weak var btnCallPublic: UIButton!
     @IBOutlet weak var btnCallFriend: UIButton!
     @IBOutlet weak var btnFiftyFifty: UIButton!
-    
-    @IBOutlet weak var labelPossibility1: UILabel!
-    @IBOutlet weak var labelPossibility2: UILabel!
-    @IBOutlet weak var labelPossibility3: UILabel!
-    @IBOutlet weak var labelPossibility4: UILabel!
+ 
     var correctAnswer: String = ""
+    var answer1: String = ""
+    var answer2: String = ""
+    var answer3: String = ""
+    var answer4: String = ""
+
     var multiplier = 0
     var currentScore = 0
     var highestScore = 0
     var currentQuestion = 0
     var arr: Questions!
+
+    var delegate: PopupDelegate? = nil
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        labelCurrentScore.text = String(currentScore)
-        labelHighestScore.text = String( highestScore)
         NetworkService.sharedObj.getQuestions{
             (Questions) in
+            self.labelCurrentScore.text = String(self.currentScore)
+            self.labelHighestScore.text = String(self.highestScore)
             self.arr = Questions.shuffled()
             self.updateQuestions()
             self.btnAnswer1.addAction(for: .touchUpInside){
@@ -105,10 +108,14 @@ class NewGameViewController: UIViewController {
     func updateQuestions() {
         btnVisibilityReset()
         self.currentQuestion = (currentQuestion + 1) % self.arr.count
-            self.btnAnswer1.setTitle(arr[self.currentQuestion].getAnswer1(), for: UIControl.State.normal)
-            self.btnAnswer2.setTitle(arr[self.currentQuestion].getAnswer2(), for: UIControl.State.normal)
-            self.btnAnswer3.setTitle(arr[self.currentQuestion].getAnswer3(), for: UIControl.State.normal)
-            self.btnAnswer4.setTitle(arr[self.currentQuestion].getAnswer4(), for: UIControl.State.normal)
+        self.answer1 = arr[self.currentQuestion].getAnswer1()
+        self.answer2 = arr[self.currentQuestion].getAnswer2()
+        self.answer3 = arr[self.currentQuestion].getAnswer3()
+        self.answer4 = arr[self.currentQuestion].getAnswer4()
+            self.btnAnswer1.setTitle(answer1, for: UIControl.State.normal)
+            self.btnAnswer2.setTitle(answer2, for: UIControl.State.normal)
+            self.btnAnswer3.setTitle(answer3, for: UIControl.State.normal)
+            self.btnAnswer4.setTitle(answer4, for: UIControl.State.normal)
             self.labelQuestion.text = arr[self.currentQuestion].getQuestion()
             self.correctAnswer = arr[self.currentQuestion].getCorrectAnswer()
     }
@@ -135,6 +142,15 @@ class NewGameViewController: UIViewController {
         let nextViewController = storyboard.instantiateViewController(withIdentifier: "GameOver")
         self.present(nextViewController, animated: true,completion: nil)
     }
+    public func getQuestionsArr()-> Questions{
+        return arr!
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVc = segue.destination as! PublicHelpViewController
+        destinationVc.popupValuePassed(answer1: self.answer1,answer2: self.answer2,answer3: self.answer3,answer4: self.answer4,correctAnswer: self.correctAnswer)
+//        helpFromPublic(Any?.self)
+    }
 }
 
 extension UIControl {
@@ -142,3 +158,4 @@ extension UIControl {
         addAction(UIAction { (action: UIAction) in closure() }, for: controlEvents)
     }
 }
+
---
+++ WhatYouKnow/WhatYouKnow/Controller/PublicHelpViewController.swift
@@ -0,0 +1,97 @@
+//
+//  PublicHelpViewController.swift
+//  WhatYouKnow
+//
+//  Created by Borislav on 5.05.22.
+//
+
+import UIKit
+class PublicHelpViewController : UIViewController,PopupDelegate{
+    @IBOutlet weak var labelAnswer1: UILabel!
+    @IBOutlet weak var labelAnswer2: UILabel!
+    @IBOutlet weak var labelAnswer3: UILabel!
+    @IBOutlet weak var labelAnswer4: UILabel!
+    private var answer1: String?
+    private var answer2: String?
+    private var answer3: String?
+    private var answer4: String?
+    private var correctAnswer: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populateValues(answer1: answer1!, answer2: answer2!, answer3: answer3!, answer4: answer4!, correctAnswer: correctAnswer!)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }    
+    
+    func populateValues(answer1:String, answer2:String, answer3:String, answer4: String, correctAnswer:String){
+        
+        var percentage = 100
+        var currentPercentage: Int
+        if(answer1 == correctAnswer){
+            currentPercentage = Int.random(in: 1...35) + 35
+            percentage -= currentPercentage
+            labelAnswer1?.text = answer1 + ":" + String(currentPercentage)
+            
+            currentPercentage = Int.random(in: 1...35)
+            percentage -= currentPercentage
+            labelAnswer2?.text = answer2 + ":" + String(currentPercentage)
+            
+            currentPercentage = Int.random(in: 1...35)
+            percentage -= currentPercentage
+            labelAnswer3?.text = answer3 + ":" + String(currentPercentage)
+            labelAnswer4?.text = answer4 + ":" + String(percentage)
+        }
+        else if(answer2 == correctAnswer){
+            currentPercentage = Int.random(in: 1...35) + 35
+            percentage -= currentPercentage
+            labelAnswer2?.text = answer2 + ":" + String(currentPercentage)
+            
+            currentPercentage = Int.random(in: 1...35)
+            percentage -= currentPercentage
+            labelAnswer1?.text = answer1 + ":" + String(currentPercentage)
+            
+            currentPercentage = Int.random(in: 1...35)
+            percentage -= currentPercentage
+            labelAnswer3?.text = answer3 + ":" + String(currentPercentage)
+            labelAnswer4?.text = answer4 + ":" + String(percentage)
+        }
+        else if(answer3 == correctAnswer){
+            currentPercentage = Int.random(in: 1...35) + 35
+            percentage -= currentPercentage
+            labelAnswer3?.text = answer3 + ":" + String(currentPercentage)
+            
+            currentPercentage = Int.random(in: 1...35)
+            percentage -= currentPercentage
+            labelAnswer2?.text = answer2 + ":" + String(currentPercentage)
+            
+            currentPercentage = Int.random(in: 1...35)
+            percentage -= currentPercentage
+            labelAnswer1?.text = answer1 + ":" + String(currentPercentage)
+            labelAnswer4?.text = answer4 + ":" + String(percentage)
+        }
+        else if(answer4 == correctAnswer){
+            currentPercentage = Int.random(in: 1...35) + 35
+            percentage -= currentPercentage
+            labelAnswer4?.text = answer4 + ":" + String(currentPercentage)
+            
+            currentPercentage = Int.random(in: 1...35)
+            percentage -= currentPercentage
+            labelAnswer2?.text = answer2 + ":" + String(currentPercentage)
+            
+            currentPercentage = Int.random(in: 1...35)
+            percentage -= currentPercentage
+            labelAnswer3?.text = answer3 + ":" + String(currentPercentage)
+            labelAnswer1?.text = answer1 + ":" + String(percentage)
+        }
+    }
+    func popupValuePassed(answer1: String, answer2: String, answer3: String, answer4: String,correctAnswer: String) {
+        self.answer1 = answer1
+        self.answer2 = answer2
+        self.answer3 = answer3
+        self.answer4 = answer4
+        self.correctAnswer = correctAnswer
+    }
+}
