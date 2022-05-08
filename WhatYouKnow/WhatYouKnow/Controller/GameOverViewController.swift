29d7251b3b92d5c7fd3d8f39559cab42f8465358
Borislav <borislav@Borislavs-Mac.local>
2022-05-08 04:50:14 +0300

    Add an enum

--- WhatYouKnow/WhatYouKnow/Controller/GameOverViewController.swift
+++ WhatYouKnow/WhatYouKnow/Controller/GameOverViewController.swift
@@ -11,19 +11,20 @@ class GameOverViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
     
     @IBAction func newGame() {
-        let newGame = NewGameViewController()
-        newGame.modalPresentationStyle = .overFullScreen
-        present(newGame, animated: true)
+        let storyBoard : UIStoryboard = UIStoryboard(name: "NewGame", bundle:nil)
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "NewGameViewController") as! NewGameViewController
+        self.present(nextViewController, animated:true, completion:nil)
+
     }
     @IBAction func homeScreen() {
-        let homeScreen = ViewController()
-        homeScreen.modalPresentationStyle = .overFullScreen
-        present(homeScreen, animated: true)
         
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        nextViewController.modalPresentationStyle = .overFullScreen
+        self.present(nextViewController, animated:true, completion:nil)
     }
 }
